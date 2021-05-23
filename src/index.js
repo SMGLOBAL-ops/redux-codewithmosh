71baa04d1afd5b3fcf7ec0eeb3c1b4fb26e57787
Saif Mustafa <68763259+SMGLOBAL-ops@users.noreply.github.com>
2021-05-23 17:44:57 +0100

    completed up to redux Store, getState, reducer intros

--- src/index.js
+++ src/index.js
@@ -1,23 +1,9 @@
-import store from './store';
+import store from './customStore';
+//import * as actions from './actions';
 
-const unsubscribe = store.subscribe(() => {
-  console.log('Store changed!', store.getState());
-});
+// store.subscribe(() => {
+//   console.log('Store Changed!');
+// });
 
-store.dispatch({
-  type: 'bugAdded',
-  payload: {
-    description: 'Bug1',
-  },
-});
-
-unsubscribe();
-
-store.dispatch({
-  type: 'bugRemoved',
-  payload: {
-    id: 1,
-  },
-});
-
-console.log(store.getState());
+//store.dispatch(actions.bugAdded('Bug 1'));
+//console.log(store.getState());
--- src/reducer.js
+++ src/reducer.js
@@ -15,3 +15,4 @@ export default function reducer(state = [], action) {
   }
   return state;
 }
+
